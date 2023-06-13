fd8ff4ead7f7502238ff825d644b68e126145f88
tzamn <tzamn@debian.tzamn>
2023-06-13 14:36:37 +0200

    add find word and find in projects short cut in telescope

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -1,6 +1,8 @@
 local telescope = require('telescope.builtin')
 
     vim.keymap.set('n', '<leader>ff', telescope.find_files, {})
+    vim.keymap.set('n', '<leader>fp', telescope.git_files, {})
     vim.keymap.set('n', '<leader>fg', telescope.live_grep, {})
+    vim.keymap.set('n', '<leader>fw', telescope.grep_string, {})
     vim.keymap.set('n', '<leader>fb', telescope.buffers, {})
     vim.keymap.set('n', '<leader>fh', telescope.help_tags, {})
