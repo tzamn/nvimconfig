41ffd2be135d8eedcb0c280a46895ce0aa25b266
Tzamn Melendez Carmona <tzamn.melendez@polito.it>
2025-06-04 14:03:58 +0200

    Remove mason-lsp-handlers, no need it in the latestes version of mason-lsp-config

--- lua/tzamn/plugins/lsp_config/mason.lua
+++ lua/tzamn/plugins/lsp_config/mason.lua
@@ -7,35 +7,10 @@ local mason_install= require("mason-tool-installer")
 local lsp_conf = require("lspconfig")
 local lsp_cmp = require("cmp_nvim_lsp")
 
-local handlers = {
-    -- The first entry (without a key) will be the default handler
-    -- and will be called for each installed server that doesn't have
-    -- a dedicated handler.
-    function (server_name) -- default handler (optional)
-        lsp_conf[server_name].setup {
-            capabilities = lsp_cmp.default_capabilities()
-        }
-    end,
-    -- Next, you can provide targeted overrides for specific servers.
-    -- ["rust_analyzer"] = function ()
-        --     require("rust-tools").setup {}
-        -- end,
-        ["lua_ls"] = function ()
-            lsp_conf.lua_ls.setup {
-                settings = {
-                    Lua = {
-                        diagnostics = {
-                            globals = { "vim" }
-                        }
-                    }
-                }
-            }
-        end,
-    }
 mason.setup({})
 
 mason_lspconf.setup({
-    -- lsps
+    automatic_enable = false,
     ensure_installed = {
         "lua_ls",
         "clangd",
@@ -53,5 +28,5 @@ mason_install.setup({
     },
 })
 
-mason_lspconf.setup_handlers(handlers)
+-- mason_lspconf.setup_handlers(handlers)
 
